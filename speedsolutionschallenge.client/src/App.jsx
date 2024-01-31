2a56fd2fa264e02c918609ca76f059774b1c128f
Gon <gonzaa1694@gmail.com>
2024-01-31 19:01:38 -0300

    create project .net core react

---
+++ speedsolutionschallenge.client/src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+    const [forecasts, setForecasts] = useState();
+
+    useEffect(() => {
+        populateData();
+    }, []);
+
+    const contents = forecasts === undefined
+        ? <p><em>Loading... Please refresh once the ASP.NET backend has started.</em></p>
+        : <table className="table table-striped" aria-labelledby="tabelLabel">
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Temp. (C)</th>
+                    <th>Temp. (F)</th>
+                    <th>Summary</th>
+                </tr>
+            </thead>
+            <tbody>
+                {forecasts.map(forecast =>
+                    <tr key={forecast.date}>
+                        <td>{forecast.date}</td>
+                        <td>{forecast.temperatureC}</td>
+                        <td>{forecast.temperatureF}</td>
+                        <td>{forecast.summary}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>;
+
+    return (
+        <div>
+            <h1 id="tabelLabel">Weather forecast</h1>
+            <p>This component demonstrates fetching data from the server.</p>
+            {contents}
+        </div>
+    );
+    
+    async function populateData() {
+        const response = await fetch('weatherforecast');
+        const data = await response.json();
+        setForecasts(data);
+    }
+}
+
+export default App;
