af58f9c44f6a3a1daff1c1f6a2571a72d37df113
Gon <gonzaa1694@gmail.com>
2024-02-05 17:38:34 -0300

    update home view adding some info

--- speedsolutionschallenge.client/src/views/Home/Home.jsx
+++ speedsolutionschallenge.client/src/views/Home/Home.jsx
@@ -3,9 +3,38 @@ import styles from './home.module.scss'
 
 const Home = () => {
   return (
-    <div className={styles.outerContainer}>
-      <div className={styles.container}>
-        Página de inicio
+      <div className={styles.outerContainer}>
+            <h2>Inicio</h2>
+          <div className={styles.container}>
+              <article>
+                  <h3>Módulos</h3>
+
+                  <li>Productos</li>
+
+                  Permite agregar, consultar, listar, modificar y borrar productos.
+
+                  <li>Precios</li>
+
+                  Permite agregar, consultar y listar precios asociados a productos.
+
+                  <li>Control de Dispensadores</li>
+
+                  Permite la creación, listado y modificación de dispensadores.
+
+                  <li>Mangueras</li>
+
+                  Permite la creación, listado, modificación y borrado de mangueras.
+
+                  <li>Visualización de Dispensadores</li>
+
+                  Se visualiza la configuración del surtidor, precios asignados, volumen, dinero, etc.
+
+                  <li>Visualización de Transacciones</li>
+
+                  Interfaz que muestra un listado de las ventas realizadas.
+                  Permite filtrar por rango de fechas y organiza el listado de forma descendente.
+
+              </article>              
       </div>
     </div>
   )
