3cda4742b4bb387c053e9d246017daa07f2c5d31
Gon <gonzaa1694@gmail.com>
2024-02-05 14:03:56 -0300

    config program.cs adding services, repositories and context

--- SpeedSolutionsChallenge.Server/Program.cs
+++ SpeedSolutionsChallenge.Server/Program.cs
@@ -1,11 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using SpeedSolutionsChallenge.Data.DBContext;
+using SpeedSolutionsChallenge.Data.Repositories.DispenserRepository;
+using SpeedSolutionsChallenge.Data.Repositories.HoseRepository;
+using SpeedSolutionsChallenge.Data.Repositories.PriceRepository;
+using SpeedSolutionsChallenge.Data.Repositories.ProductRepository;
+using SpeedSolutionsChallenge.Server.Services.DispenserService;
+using SpeedSolutionsChallenge.Server.Services.HoseService;
+using SpeedSolutionsChallenge.Server.Services.PriceService;
+using SpeedSolutionsChallenge.Server.Services.ProductService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IPriceRepository, PriceRepository>();
+builder.Services.AddScoped<IDispenserRepository, DispenserRepository>();
+builder.Services.AddScoped<IHoseRepository, HoseRepository>();
+
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IPriceService, PriceService>();
+builder.Services.AddScoped<IDispenserService, DispenserService>();
+builder.Services.AddScoped<IHoseService, HoseService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
